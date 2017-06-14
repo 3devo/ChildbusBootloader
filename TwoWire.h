13984115db52febcb0242e11f2fc202f2bec18cc
Matthijs Kooijman <matthijs@stdin.nl>
2017-06-14 14:35:25 +0200

    Add license to each source file
    This also removes the existing header containing the filename, creation
    date and username of the initial author, which do not really add any
    value.

--- TwoWire.h
+++ TwoWire.h
@@ -1,10 +1,19 @@
 /*
- * TwoWire.h
+ * Copyright (C) 2015-2017 Erin Tomson <erin@rgba.net>
  *
- * Created: 7/16/2015 7:19:10 PM
- *  Author: ekt
- */ 
-
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef TWOWIRE_H_
 #define TWOWIRE_H_
--- TwoWire841.cpp
+++ TwoWire841.cpp
@@ -1,9 +1,19 @@
 /*
- * TwoWire841.cpp
+ * Copyright (C) 2015-2017 Erin Tomson <erin@rgba.net>
  *
- * Created: 7/16/2015 7:16:26 PM
- *  Author: ekt
- */ 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #if defined(__AVR_ATtiny841__) || defined(__AVR_ATtiny441__)
 
