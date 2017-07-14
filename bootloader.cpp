94142aedac15211f95fef267061333ba2d56578c
Matthijs Kooijman <matthijs@stdin.nl>
2017-07-14 18:14:27 +0200

    Handle the framing of commands and replies
    This adds a CRC check, calls `processCommand()` on the contained command,
    and adds a CRC to the reply. `processCommand()` is still mostly empty, it
    now returns `COMMAND_NOT_SUPPORTED` for all commands.

--- bootloader.cpp
+++ bootloader.cpp
@@ -16,9 +16,29 @@
  */
 
 #include <string.h>
+#include <util/crc16.h>
 
 #include "TwoWire.h"
 #include "SelfProgram.h"
+
+struct Status {
+	static const uint8_t COMMAND_OK            = 0x00;
+	static const uint8_t COMMAND_FAILED        = 0x01;
+	static const uint8_t COMMAND_NOT_SUPPORTED = 0x02;
+	static const uint8_t INVALID_TRANSFER      = 0x03;
+	static const uint8_t INVALID_CRC           = 0x04;
+	static const uint8_t INVALID_ARGUMENTS     = 0x05;
+};
+
+struct Commands {
+	static const uint8_t GET_PROTOCOL_VERSION  = 0x00;
+	static const uint8_t SET_I2C_ADDRESS       = 0x01;
+	static const uint8_t POWER_UP_DISPLAY      = 0x02;
+	static const uint8_t GET_HARDWARE_INFO     = 0x03;
+	static const uint8_t START_APPLICATION     = 0x04;
+	static const uint8_t WRITE_FLASH           = 0x05;
+	static const uint8_t FINALIZE_FLASH        = 0x06;
+};
 
 SelfProgram selfProgram;
 volatile bool bootloaderRunning = true;
@@ -31,8 +51,44 @@ uint32_t getUInt32(uint8_t *data) {
 	return data[0] | (data[1] << 8) | ((uint32_t)data[2] << 16) | ((uint32_t)data[3] << 24);
 }
 
+static uint8_t calcCrc(uint8_t *data, uint8_t len) {
+	uint8_t crc = 0;
+	for (uint8_t i = 0; i < len; ++i)
+		crc = _crc8_ccitt_update(crc, data[i]);
+	return crc;
+}
+
+static int processCommand(uint8_t *data, uint8_t len, uint8_t maxLen) {
+	switch (data[0]) {
+		default:
+			data[0] = Status::COMMAND_NOT_SUPPORTED;
+			return 1;
+	}
+}
+
 int TwoWireCallback(uint8_t address, uint8_t *data, uint8_t len, uint8_t maxLen) {
-	return 0;
+	// Check that there is at least room for a status byte and a CRC
+	if (maxLen < 2)
+		return 0;
+
+	if (len < 2) {
+		data[0] = Status::INVALID_TRANSFER;
+		len = 1;
+	} else {
+		uint8_t crc = calcCrc(data, len);
+		if (crc != 0) {
+			data[0] = Status::INVALID_CRC;
+			len = 1;
+		} else {
+			// CRC checks out, process a command
+			len = processCommand(data, len - 1, maxLen - 1);
+		}
+	}
+
+	data[len] = calcCrc(data, len);
+	++len;
+
+	return len;
 }
 
 
